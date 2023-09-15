61aacabde73ab760afac6811174f2106d322509b
alvarorichard <alvaro10048@protonmail.com>
2023-09-15 18:28:30 -0300

    add test

--- src/test.rs
+++ src/test.rs
@@ -1,14 +1,9 @@
 extern crate reqwest;
-extern crate scraper;
-
-use lazy_regex::regex_captures;
+use regex::Regex;
 use std::io;
-
-use scraper::{Html, Selector};
 use tokio;
 
 #[tokio::main]
-
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Por favor, insira o nome do anime:");
 
@@ -16,11 +11,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     io::stdin().read_line(&mut anime_name)?;
     let anime_name = anime_name.trim().to_string();
 
-    // Formatar o nome do anime para ser URL-friendly (ex: substituir espaços por -)
     let search_term = anime_name.replace(" ", "-").to_lowercase();
 
     let api_url = "https://betteranime.net/pesquisa";
-
 
     let client = reqwest::Client::new();
     let response = client
@@ -32,8 +25,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     if response.status().is_success() {
         let data = response.text().await?;
-        let found = regex_captures!(r#""(https:\/\/betteranime\.net\/anime\/([\w]+)\/([\w-]+))""#, &data);
-        println!("{:?}", &found);
+        let re = Regex::new(r#""(https:\/\/betteranime\.net\/anime\/([\w]+)\/([\w-]+))""#).unwrap();
+        let found: Vec<_> = re.captures_iter(&data).collect();
+
+        println!("Opções encontradas:");
+        for (i, cap) in found.iter().enumerate() {
+            println!("{}: {}", i + 1, &cap[1]);
+        }
+
+        println!("Por favor, selecione uma opção:");
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice)?;
+        let choice: usize = choice.trim().parse()?;
+
+        if choice > 0 && choice <= found.len() {
+            let selected_url = &found[choice - 1][1];
+            println!("Você selecionou: {}", selected_url);
+            // Aqui você pode adicionar o código para dar play no vídeo
+        } else {
+            println!("Opção inválida.");
+        }
     } else {
         println!("Erro ao fazer a requisição: {}", response.status());
     }
